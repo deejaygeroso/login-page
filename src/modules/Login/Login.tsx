b429d58f19bdbda1d4a5d1be9c71272d2563428e
Deejay Geroso <deejaygeroso@gmail.com>
2020-09-04 10:52:13 +0800

    login page initial design

--- src/modules/Login/Login.tsx
+++ src/modules/Login/Login.tsx
@@ -1,21 +1,41 @@
+import { FiMail, IoMdPerson, RiLockPasswordFill } from 'react-icons/all'
 import React, { FunctionComponent, ReactElement } from 'react'
 import './styles.css'
 
 const Login: FunctionComponent = (): ReactElement => {
   return (
     <div id='login-page'>
-      <div id='login-form'>
-        <div>
-          <input id='firstName' placeholder='First Name' type='text' />
-        </div>
-        <div>
-          <input id='lastName' placeholder='Last Name' type='text' />
-        </div>
-        <div>
-          <input id='email' placeholder='Email' type='email' />
-        </div>
-        <div>
-          <input id='password' placeholder='Password' type='password' />
+      <div id='login-form-container'>
+        <div id='login-form'>
+          <div className='input-container'>
+            <div>
+              <IoMdPerson />
+              <input id='firstName' placeholder='First Name' type='text' />
+            </div>
+            <p className='error-message'></p>
+          </div>
+          <div className='input-container'>
+            <div>
+              <IoMdPerson />
+              <input id='lastName' placeholder='Last Name' type='text' />
+            </div>
+            <p className='error-message'></p>
+          </div>
+          <div className='input-container'>
+            <div>
+              <FiMail />
+              <input id='email' placeholder='Email' type='email' />
+            </div>
+            <p className='error-message'></p>
+          </div>
+          <div className='input-container'>
+            <div>
+              <RiLockPasswordFill />
+              <input id='password' placeholder='Password' type='password' />
+            </div>
+            <p className='error-message'></p>
+          </div>
+          <button className='submit-button'>Submit</button>
         </div>
       </div>
     </div>
